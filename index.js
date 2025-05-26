87778f69a353fe829c20250327561692b5401f7d
sebastian438 <sebastian07051999@gmail.com>
2025-05-26 16:06:57 +0200

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 //DESARROLLA AQUI TUS SOLUCIONES
 
-async function getImageAndName (pokemon){
+//EJERCICIO 02
+
+async function getImageAndName(pokemon) {
 
     let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     let data = await response.json();
     let name = data.name;
     let img = data.sprites.front_default;
-    return {name, img}
-    
+    return { name, img }
+
+}
+
+
+//EJERCICIO 01
+
+const getRandomPokemon = async () => {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/`);
+    let data = await response.json();
+    let count = data.count;
+    console.log(count);
+    let randomId = Math.floor(Math.random() * count);
+    console.log(randomId);
+    let information = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
+    let datos = await information.json();
+    console.log(datos);
+    return datos;
 }
